ba7222ce927ab8ac84743ddc7397615b1498d4ba
朱景辉 <jinghui.zhu@luckincoffee.com>
2019-05-15 11:32:53 +0800

    LeetCode 17 电话号码的字母组合 设计模式-简单工厂模式

---
+++ src/pers/design_pattern/factory/Dog.java
@@ -0,0 +1,15 @@
+package pers.design_pattern.factory;
+
+/**
+ * ${todo}
+ *
+ * @author jinghui.zhu@luckincoffee.com
+ * @date 2019/5/14 17:18
+ */
+public class Dog implements Animal {
+
+    @Override
+    public void sound() {
+        System.out.println("狗的叫声: '汪汪汪'");
+    }
+}
