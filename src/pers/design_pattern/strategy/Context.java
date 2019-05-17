0edaebc1edd7dc0b6b770bccda07fc9207da227f
朱景辉 <jinghui.zhu@luckincoffee.com>
2019-05-17 14:30:06 +0800

    设计模式 -- 策略模式

---
+++ src/pers/design_pattern/strategy/Context.java
@@ -0,0 +1,20 @@
+package pers.design_pattern.strategy;
+
+/**
+ * Context 类
+ *
+ * @author jinghui.zhu@luckincoffee.com
+ * @date 2019/5/17 14:23
+ */
+public class Context {
+
+    private Strategy strategy;
+
+    public Context(Strategy strategy){
+        this.strategy = strategy;
+    }
+
+    public void executeStrategy(){
+        strategy.useStrategy();
+    }
+}
