74d6cba9f3105b22821dbcefc0182ac68f9434f4
朱景辉 <jinghui.zhu@luckincoffee.com>
2020-03-30 10:45:02 +0800

    add ignore

--- src/pers/zjh/pratice/stringtest/NewStringTest.java
+++ src/pers/zjh/pratice/stringtest/NewStringTest.java
@@ -1,10 +1,32 @@
 package pers.zjh.pratice.stringtest;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 /**
  * Created by jinghui.zhu on 2018/10/30
  */
 public class NewStringTest {
     public static void main(String[] args) {
-        String s = new String("abc");
+//        long id=1001;
+//        StringBuffer sb = new StringBuffer();
+//        sb.append(LongToFixLengthString(id));
+//        //sb.append(" ");
+//        SimpleDateFormat df = new SimpleDateFormat("yyMMddHHmm");
+//        sb.append(df.format(new Date()));
+//        //sb.append(" ");
+//        Random random = new Random();
+//        sb.append(random.nextInt(90)+10);
+//        System.out.println(sb.toString());
+
+        System.out.println(LongToFixLengthString(23233));
+    }
+
+    public static String LongToFixLengthString(long id) {
+        NumberFormat formatter = new DecimalFormat("00000000");
+        return formatter.format(id);
     }
 }
---
+++ src/pers/zjh/pratice/stringtest/NewTest.java
@@ -0,0 +1,42 @@
+package pers.zjh.pratice.stringtest;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * ${todo}
+ *
+ * @author jinghui.zhu@luckincoffee.com
+ * @date 2019/8/23 17:34
+ */
+public class NewTest {
+
+    public static void main(String[] args) {
+        int denom = 10;
+        int num = 4;
+        double ratio;
+
+        ratio = denom == 106 ? 43543 : num / denom;
+        System.out.println("ratio = " + ratio);
+
+
+//        Date date = new Date();
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMM");
+//        System.out.println(simpleDateFormat.format(date));
+
+//
+//        Calendar cal = Calendar.getInstance();
+//        SimpleDateFormat dft = new SimpleDateFormat("yyyyMM");
+//        String lastMonth = dft.format(cal.getTime());
+//        System.out.println(lastMonth);
+
+//        DecimalFormat df = new DecimalFormat("#.00");
+//        System.out.println(df.format(3));
+
+
+//        String a = new DecimalFormat("00000000").format(23);
+//        System.out.println(a);
+    }
+}
