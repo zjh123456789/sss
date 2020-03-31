72f01af0fc6063be32a7e1556862a32bcbe531a3
jinghui.zhu <jinghui.zhu@luckincoffee.com>
2020-03-31 21:42:00 +0800

    求字符串中含有的不同字符的个数

---
+++ src/pers/zjh/algorithm/Test.java
@@ -0,0 +1,35 @@
+package pers.zjh.algorithm;
+
+import java.util.Scanner;
+
+/**
+ * 计算字符串中含有的不同字符的个数，字符在 ACSII 码范围内（0~127），不在范围的不作统计
+ *
+ * @author 朱景辉
+ * @createTime 2020/3/31 21:07
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        while(scanner.hasNext()){
+            String s=scanner.nextLine();
+            int len=getLen(s);
+            System.out.println(len);
+        }
+    }
+
+    public static int getLen(String s) {
+        int[] arr = new int[128];
+        for(int i = 0; i < s.length(); i++){
+            arr[s.charAt(i)] = 1;
+        }
+        int len=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == 1){
+                len++;
+            }
+        }
+        return len;
+    }
+}
