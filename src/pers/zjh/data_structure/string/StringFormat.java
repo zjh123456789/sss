4dd0024b4cfe0aba56bd3b3dfe88fd56dc243e28
朱景辉 <jinghui.zhu@luckincoffee.com>
2020-04-01 15:44:31 +0800

    字符串格式化

---
+++ src/pers/zjh/data_structure/string/StringFormat.java
@@ -0,0 +1,14 @@
+package pers.zjh.data_structure.string;
+
+/**
+ * 字符串格式化
+ *
+ * @author jinghui.zhu@luckincoffee.com
+ * @date 2020/4/1 11:28:04
+ */
+public class StringFormat {
+
+    public static void main(String[] args) {
+        System.out.println(String.format("express_settle:%s", 33));
+    }
+}
