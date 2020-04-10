9e2acb7c8f628e448d1ef31bc8f2a75760001e9b
jinghui.zhu <jinghui.zhu@luckincoffee.com>
2020-04-10 09:14:30 +0800

    joda-time Datetime 测试类

--- src/pers/zjh/tools/Timing.java
+++ src/pers/zjh/tools/Timing.java
@@ -17,8 +17,25 @@ public class Timing {
         // 本月第一天
         DateTime periodEndTime = new DateTime(new Date()).withDayOfMonth(1).withTime(0, 0, 0, 0);
 
-        Date date = new DateTime(new Date()).minusMonths(1).toDate();
+        System.out.println("ssss " + periodStartTime);
 
-        System.out.println("ssss");
+        // 当前时间
+        Date now = new Date();
+        System.out.println("当前时间: " + now);
+
+        // 昨天 dateTime
+        DateTime yesterday = new DateTime(now).minusDays(1);
+        System.out.println("昨天 dateTime: " + yesterday);
+        // 昨天 date
+        Date yesterdayDate = yesterday.toDate();
+        System.out.println("昨天 date: " + yesterdayDate);
+
+        long days = now.getTime() - yesterday.toDate().getTime();
+        System.out.println("间隔时长: " + days);
+        int tian = (int) (days / (1400 * 60 * 60 * 24));
+        System.out.println("天数: " + tian);
+
+
+        System.out.println("一天包含多少秒: " + 24*3600);
     }
 }
