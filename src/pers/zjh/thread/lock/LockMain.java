08f85088879253cc1da57a23799135993068329b
jinghui.zhu <jinghui.zhu@luckincoffee.com>
2020-04-19 09:53:01 +0800

    对比 lock() 和 lockInterruptibly()

---
+++ src/pers/zjh/thread/lock/LockMain.java
@@ -0,0 +1,42 @@
+package pers.zjh.thread.lock;
+
+/**
+ * main
+ *
+ * @author 朱景辉
+ * @createTime 2020/4/19 9:10
+ */
+public class LockMain {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final LockTest lockTest = new LockTest();
+
+        Thread t0 = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        lockTest.doBusiness();
+                    }
+                }
+        );
+
+        Thread t1 = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        lockTest.doBusiness();
+                    }
+                }
+        );
+
+        // 启动线程t0
+        t0.start();
+        Thread.sleep(10);
+        // 启动线程t1
+        t1.start();
+        Thread.sleep(100);
+        // 线程t1未获取到锁,中断t1的等待
+        t1.interrupt();
+    }
+}
