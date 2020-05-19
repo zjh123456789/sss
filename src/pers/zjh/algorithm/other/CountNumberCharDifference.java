4eda35513a78b597e7971cda7deffa5452aa1649
jinghui.zhu <jinghui.zhu@luckincoffee.com>
2020-05-19 13:23:54 +0800

    统计字符串中不同字符个数 set

--- src/pers/zjh/algorithm/other/CountNumberCharDifference.java
+++ src/pers/zjh/algorithm/other/CountNumberCharDifference.java
@@ -1,6 +1,8 @@
 package pers.zjh.algorithm.other;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * 计算字符串中含有的不同字符的个数，字符在 ACSII 码范围内（0~127），不在范围的不作统计
@@ -8,17 +10,22 @@ import java.util.Scanner;
  * @author 朱景辉
  * @createTime 2020/3/31 21:07
  */
-public class Test {
+public class CountNumberCharDifference {
 
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         while(scanner.hasNext()){
             String s = scanner.nextLine();
-            int len = getLen(s);
+            int len = method2(s);
             System.out.println(len);
         }
     }
 
+    /**
+     * 利用 ASCII 数组去重
+     * @param s 字符串
+     * @return  不同字符个数
+     */
     private static int getLen(String s) {
         int[] arr = new int[128];
         for(int i = 0; i < s.length(); i++){
@@ -32,4 +39,21 @@ public class Test {
         }
         return len;
     }
+
+    /**
+     * set 自动去重
+     * @param s     字符串
+     * @return      不同字符个数
+     */
+    private static int method2(String s) {
+        Set<Character> set = new HashSet(128);
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (c <= 127) {
+                set.add(c);
+            }
+        }
+        return set.size();
+    }
+
 }
